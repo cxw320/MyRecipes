a2613062721b6ba55630b8aea78351c14130e6aa
Caroline Wang <cxw320@case.edu>
2023-06-07 11:53:38 -0400

    Add some code to print out result of retrofit

--- app/src/main/java/com/example/myrecipes/MainActivity.kt
+++ app/src/main/java/com/example/myrecipes/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.myrecipes
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.lifecycleScope
+import com.example.myrecipes.api.SpoonacularApiService
+import com.example.myrecipes.api.responsemodel.RecipeDTO
+import com.example.myrecipes.model.Recipe
 import com.example.myrecipes.screens.recipediscovery.RecipeDiscoveryScreen
 import com.example.myrecipes.ui.theme.MyRecipesTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var spoonacularApi : SpoonacularApiService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //TESTING the api to make sure we didn't mess up *fingers crossed* =D !!!!
+        lifecycleScope.launch {
+            val recipeList = spoonacularApi.getRandomRecipes().body()?.recipes?.map {
+                mapToRecipeModel(it)
+            } ?: listOf(Recipe())
+            Log.d("Caroline","${recipeList[0]}")
+        }
         setContent {
             MyRecipesTheme {
                 // A surface container using the 'background' color from the theme
@@ -26,6 +45,14 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    fun mapToRecipeModel(response: RecipeDTO): Recipe {
+        return Recipe(
+            id = response.id,
+            title = response.title,
+            imageUrl = response.imageUrl
+        )
     }
 
 }
--- app/src/main/java/com/example/myrecipes/MyRecipesHiltApp.kt
+++ app/src/main/java/com/example/myrecipes/MyRecipesHiltApp.kt
@@ -1,4 +1,4 @@
-package com.example
+package com.example.myrecipes
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
