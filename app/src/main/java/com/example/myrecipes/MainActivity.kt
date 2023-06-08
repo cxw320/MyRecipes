4552e3ba492ff2e9f743367708cc84c8341dc3cc
Caroline Wang <cxw320@case.edu>
2023-06-08 15:00:12 -0400

    add collectAsState to ui state in recipe discovery screen composable

--- app/src/main/java/com/example/myrecipes/MainActivity.kt
+++ app/src/main/java/com/example/myrecipes/MainActivity.kt
@@ -1,22 +1,15 @@
 package com.example.myrecipes
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.lifecycleScope
-import com.example.myrecipes.api.SpoonacularApiService
-import com.example.myrecipes.api.responsemodel.RecipeDTO
-import com.example.myrecipes.model.Recipe
 import com.example.myrecipes.screens.recipediscovery.RecipeDiscoveryScreen
 import com.example.myrecipes.ui.theme.MyRecipesTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -31,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                   // RecipeDiscoveryScreen()
+                    RecipeDiscoveryScreen()
                 }
             }
         }
--- app/src/main/java/com/example/myrecipes/screens/recipediscovery/RecipeDiscoveryViewModel.kt
+++ app/src/main/java/com/example/myrecipes/screens/recipediscovery/RecipeDiscoveryViewModel.kt
@@ -1,11 +1,14 @@
 package com.example.myrecipes.screens.recipediscovery
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.myrecipes.model.Recipe
 import com.example.myrecipes.repository.RecipeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
+import java.util.concurrent.Flow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,11 +16,17 @@ class RecipeDiscoveryViewModel @Inject constructor(
     private val recipeRepository: RecipeRepository
 ) : ViewModel() {
 
-    private var recipeList = emptyList<Recipe>()
+    val uiState = MutableStateFlow(RecipeDiscoveryUiState())
 
     init {
         viewModelScope.launch{
-            recipeList = recipeRepository.getRandomRecipes()
+            val recipeList = recipeRepository.getRandomRecipes()
+            Log.d("Caroline","recipe list is $recipeList")
+            uiState.emit(
+                uiState.value.copy(
+                    recipeList = recipeList
+                )
+            )
         }
     }
 
