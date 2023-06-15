c849aa8cf1eb4be6f77c4b89359b13e45e94b8ee
Caroline Wang <cxw320@case.edu>
2023-06-15 15:29:11 -0400

    Add new branch for setting up retrofit with no hilt

--- app/src/main/java/com/example/myrecipes/MainActivity.kt
+++ app/src/main/java/com/example/myrecipes/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myrecipes
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,12 +11,26 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.example.myrecipes.api.SpoonacularApi
+import com.example.myrecipes.api.responsemodel.RecipeDTO
+import com.example.myrecipes.model.Recipe
 import com.example.myrecipes.screens.recipediscovery.RecipeDiscoveryScreen
 import com.example.myrecipes.ui.theme.MyRecipesTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Let's test that we can Log some data from the spoonacular api!
+        //If we did everything correctly, we should see the first item in the recipe list printed in our Logcat
+        lifecycleScope.launch{
+            val recipeList = SpoonacularApi.apiService.getRandomRecipes().body()?.recipes?.map {
+                mapToRecipeModel(it)
+            } ?: listOf(Recipe())
+            Log.d("Caroline","${recipeList[0]}")
+        }
         setContent {
             MyRecipesTheme {
                 // A surface container using the 'background' color from the theme
@@ -27,5 +42,14 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    fun mapToRecipeModel(response: RecipeDTO): Recipe {
+
+        return Recipe(
+            id = response.id,
+            title = response.title,
+            imageUrl = response.imageUrl
+        )
     }
 }
---
+++ app/src/main/java/com/example/myrecipes/api/interceptors/SpoonacularInterceptor.kt
@@ -0,0 +1,18 @@
+package com.example.myrecipes.api.interceptors
+
+import com.example.myrecipes.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class SpoonacularInterceptor : Interceptor{
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val apiKey = BuildConfig.SPOONACULAR_API_KEY
+        val url = chain.request().url.newBuilder().addQueryParameter("apiKey",apiKey).build()
+        val newRequest = chain.request()
+            .newBuilder()
+            .url(url)
+            .build()
+        return chain.proceed(newRequest)
+    }
+
+}
