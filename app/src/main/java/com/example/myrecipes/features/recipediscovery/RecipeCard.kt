67cd405ddee143226a4cf6b0af2745af716c15bb
Caroline Wang <cxw320@case.edu>
2023-06-26 19:21:06 -0400

    Add preview for recipe card

--- app/src/main/java/com/example/myrecipes/features/recipediscovery/RecipeCard.kt
+++ app/src/main/java/com/example/myrecipes/features/recipediscovery/RecipeCard.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.myrecipes.R
@@ -37,4 +38,12 @@ fun RecipeCard(
             text = recipe.title
         )
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RecipeCardPreview() {
+    RecipeCard(
+        Recipe(1,"Cinnamon Rolls","","Tasty rolls")
+    )
 }
