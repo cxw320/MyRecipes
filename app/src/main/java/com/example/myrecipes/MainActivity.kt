f615790503015d346f8d1a16794d5348cbef0742
Caroline Wang <cxw320@case.edu>
2023-06-27 03:21:46 -0400

    move the retrofit instantiation code into main activity

--- app/src/main/java/com/example/myrecipes/MainActivity.kt
+++ app/src/main/java/com/example/myrecipes/MainActivity.kt
@@ -9,21 +9,57 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.lifecycleScope
-import com.example.myrecipes.api.SpoonacularApi
+import com.example.myrecipes.api.ApiService
+import com.example.myrecipes.api.interceptors.SpoonacularInterceptor
 import com.example.myrecipes.api.responsemodel.RecipeDTO
 import com.example.myrecipes.model.Recipe
 import com.example.myrecipes.features.recipediscovery.RecipeDiscoveryScreen
 import com.example.myrecipes.ui.theme.MyRecipesTheme
+import com.squareup.moshi.Moshi
 import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //The below section creates our Retrofit instance and will need to eventually be moved out of main activity
+        //This section will eventually be moved into a repository (but in future branches, we will also discuss hiding this instantiation code into a dependency injection library
+
+        val BASE_URL = "https://api.spoonacular.com/recipes/"
+
+        //We use a client builder to modify our request to include an api key
+        val client: OkHttpClient = OkHttpClient.Builder()
+            .addInterceptor(SpoonacularInterceptor())
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            //we need to provide a base url
+            .baseUrl(BASE_URL)
+            // we need to tell Retrofit how to convert the format of the data it gets back
+            .addConverterFactory(
+                MoshiConverterFactory.create(
+                    Moshi.Builder().build()
+                )
+            )
+            //You also have the option of passing in an OkHttpClient instance during initialization to customize behavior
+            //Customization includes: setting time-outs,enabling caching
+            //The other advantage is defining interceptors which can modify requests or responses
+            //In this case, we want to take advantage of an interceptor to add an api key to our request
+            .client(client)
+            .build()
+
+
         //Let's test that we can Log some data from the spoonacular api!
         //If we did everything correctly, we should see the first item in the recipe list printed in our Logcat
         lifecycleScope.launch{
-            val recipeList = SpoonacularApi.apiService.getRandomRecipes().recipes.map {
+            val recipeList = retrofit
+                .create(ApiService::class.java)
+                .getRandomRecipes()
+                .recipes
+                .map {
                 mapToRecipeModel(it)
             }
             Log.d("Caroline","${recipeList[0]}")
--- app/src/main/java/com/example/myrecipes/api/ApiService.kt
+++ app/src/main/java/com/example/myrecipes/api/ApiService.kt
@@ -16,36 +16,3 @@ interface ApiService {
     suspend fun getRandomRecipes(): RecipeListResponse
 
 }
-
-//The below section will eventually be moved to our repository (its not common in android development to instantiate these services in the same file)
-
-private const val BASE_URL = "https://api.spoonacular.com/recipes/"
-
-//We use a client builder to modify our request to include an api key
-val client: OkHttpClient = OkHttpClient.Builder()
-    .addInterceptor(SpoonacularInterceptor())
-    .build()
-
-private val retrofit = Retrofit.Builder()
-    //we need to provide a base url
-    .baseUrl(BASE_URL)
-    // we need to tell Retrofit how to convert the format of the data it gets back
-    .addConverterFactory(
-        MoshiConverterFactory.create(
-            Moshi.Builder().build()
-        )
-    )
-    //You also have the option of passing in an OkHttpClient instance during initialization to customize behavior
-    //Customization includes: setting time-outs,enabling caching
-    //The other advantage is defining interceptors which can modify requests or responses
-    //In this case, we want to take advantage of an interceptor to add an api key to our request
-    .client(client)
-    .build()
-
-//Creating this so I can access this in the main activity for my "test" since object can be accessed globally without needing to instantiate
-object SpoonacularApi {
-    val apiService: ApiService by lazy {
-        retrofit.create(ApiService::class.java)
-    }
-}
-
